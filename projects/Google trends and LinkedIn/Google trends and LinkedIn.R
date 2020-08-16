ac836f1c8be7961d0e2abdd5891787796e11d089
Nir Levy <nirlevy@fb.com>
2020-08-16 12:53:06 +0300

    Adding LinkedIn

--- projects/Google trends and LinkedIn/Google trends and LinkedIn.R
+++ projects/Google trends and LinkedIn/Google trends and LinkedIn.R
@@ -4,12 +4,19 @@
 
 # installing the packages
 # install.packages("gtrendsR")
-#install.packages("Rlinkedin")
+# install.packages("httr")
+# install.packages("devtools")
+# install.packages("Rlinkedin")
+# install_github("mpiccirilli/Rlinkedin")
 
-## loading the library 
-library(gtrendsR)
+## loading the libraries 
 library(Rlinkedin)
+library(gtrendsR)
+library(devtools)
+library(httr)
 
+
+# Google trends
 # setting the time period
 time_period<-"2016-01-01 2018-07-18"
 
@@ -32,3 +39,50 @@ plot(res_trend)
 res_trend <- gtrends(c("הכשרה מקצועית"),geo=c("IL"), time=time_period)
 plot(res_trend)
 
+
+# LinkedIn
+
+in.auth <- inOAuth()
+app_name <- "xxx"
+consumer_key <- "xxx"
+consumer_secret <- "xxx"
+
+in.auth <- inOAuth(app_name, consumer_key, consumer_secret)
+
+
+endpoints <- oauth_endpoints("linkedin")
+endpoints
+
+myapp <- oauth_app("linkedin",
+                   key = "xxx",
+                   secret = "XXX"
+)
+
+token <- oauth2.0_token(endpoints, myapp, scope = "r_liteprofile")
+
+req <- GET("https://api.linkedin.com/v2/me", config(token = token))
+stop_for_status(req)
+content(req)
+
+# https://github.com/r-lib/httr/blob/master/demo/oauth2-linkedin.r
+
+
+search.results <- searchJobs(token = in.auth, keywords = "נהג אוטובוס", country="IL")
+colnames(search.results)
+nrow(search.results)
+
+search.results <- searchJobs(token = in.auth, keywords = "נהג מונית", country="IL")
+colnames(search.results)
+nrow(search.results)
+
+search.results <- searchJobs(token = in.auth, keywords = "נהג הסעות", country="IL")
+colnames(search.results)
+nrow(search.results)
+
+search.results <- searchJobs(token = in.auth, keywords = "Designer", country="IL")
+colnames(search.results)
+nrow(search.results)
+
+search.results <- searchJobs(token = in.auth, keywords = "UX Designer", country="IL")
+colnames(search.results)
+nrow(search.results)
