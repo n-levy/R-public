768356dad0748146383479e21fd778ec05e66732
n-levy <nirlevy5@gmail.com>
2021-11-23 22:55:57 +0100

    testing upload from local laptop

--- projects/Harvardx/Capstone - movielens.R
+++ projects/Harvardx/Capstone - movielens.R
@@ -3,7 +3,7 @@
 ##########################################################
 
 # Note: this process could take a couple of minutes
-#test
+# testing upload from local rstudio
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
