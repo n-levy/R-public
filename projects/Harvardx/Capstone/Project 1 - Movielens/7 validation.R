c68d076a0dfc2a29d9b21acd21da4e9dd69c15d1
Nir Levy <nirlevy5@gmail.com>
2021-12-04 22:29:22 +0100

    validation

--- projects/Harvardx/Capstone/Project 1 - Movielens/7 validation.R
+++ projects/Harvardx/Capstone/Project 1 - Movielens/7 validation.R
@@ -1,45 +1,61 @@
+##################
 ### validation ###
+##################
 
+### Loading libraries ###
+library(tidyverse)
+library(caret)
+# library(data.table)
+library(dplyr)
+# library(kableExtra)
+library(recommenderlab)
+library(Matrix)
+# library(BBmisc)
+# library(DT)
+# library(pander)
+
+# cleaning the environment
+# rm(list = ls())
+rm(edx, result_rating_svdf_10, samp, scheme_10, testmat_full, users_and_ratings_test_set)
+
+### Increasing memory
+# Checking memory limit
+# memory.limit()
+# Change memory limit
+# memory.limit(size = 10^9)
+
+# cleaning memory
+invisible(gc())
+
+# converting the testing set into a matrix
+users_and_ratings_test_set<-cbind.data.frame(validation$userId, validation$movieId, validation$rating)
+dim(users_and_ratings_test_set)
+# head(users_and_ratings_test_set)
 
 # converting the matrix into a "realRatingMatrix")
-trainmat_full <- as(users_and_ratings_training_set, "realRatingMatrix")
-dim(trainmat_full)
-
-# class(trainmat)
-
-# removing items with few ratings because of low confidence in these ratings
-min_n_movies <- quantile(rowCounts(trainmat_full), 0.9)
-print(min_n_movies)
-
-min_n_users <- quantile(colCounts(trainmat_full), 0.9)
-print(min_n_users)
-
-trainmat <- trainmat_full[rowCounts(trainmat_full) > min_n_movies,
-                          colCounts(trainmat_full) > min_n_users]
-
-dim(trainmat_full)
-dim(trainmat)
+testmat <- as(users_and_ratings_test_set, "realRatingMatrix")
+dim(testmat)
 
 # checking number of ratings per item
-number_of_ratings<-colCounts(trainmat)
+number_of_ratings<-colCounts(testmat)
 min(number_of_ratings)
 max(number_of_ratings)
 
 # exploring the matrix
-dim(trainmat)
-# trainmat@data[1500:1510, 2001:2009]
+dim(testmat)
+# testmat@data[1500:1510, 2001:2009]
 
 # normalizing the values
-# normalize(trainmat, method = "Z-score")
+# normalize(testmat, method = "Z-score")
 
 # saving
-# saveRDS(trainmat, file="trainmat")
+# saveRDS(testmat, file="testmat")
 
 # checking the initial/default parameters of the SVDF model
 # recommenderRegistry$get_entry("SVDF", dataType = "realRatingMatrix")
 
 # recommending
-# recom_svdf <- Recommender(data = trainmat,
+# recom_svdf <- Recommender(data = testmat,
 #                           method = "SVDF",
 #                           parameter = list(normalize = "Z-score")
 # )
@@ -48,10 +64,10 @@ dim(trainmat)
 set.seed(123, sample.kind="Rounding")
 
 # Setting up the evaluation scheme
-scheme_10 <- trainmat %>% 
+scheme_test <- testmat %>% 
   evaluationScheme(method = "split",
                    k=1,
-                   train  = 0.9,  # 90% data train
+                   test  = 0.9,  # 90% data test
                    given  = -8,
                    goodRating = 3
   )
@@ -100,7 +116,7 @@ result_rating_svdf_10@results %>%
   summarise_all(mean) %>% 
   setNames(c("RMSE", "MSE", "MAE"))
 # items_to_recommend<-10
-# UBCF_prediction <- predict(object = trainmat, newdata = testmat, n = items_to_recommend, 
+# UBCF_prediction <- predict(object = testmat, newdata = testmat, n = items_to_recommend, 
 #                              type = "ratings")
 rm(edx)
 rm(users_and_ratings)
@@ -108,9 +124,9 @@ rm(validation)
 rm(user_avgs)
 rm(movie_avgs, movie_reg_avgs)
 rm(testmat)
-rm(trainmat_full)
+rm(testmat_full)
 rm(samp)
-rm(users_and_ratings_training_set)
+rm(users_and_ratings_test_set)
 # rm(list = ls())
 
 # Source 1: https://rpubs.com/Argaadya/recommender-svdf
