dce7ad2d32835e8cf20b8a75db013a1889d8ff7b
Nir Levy <nirlevy5@gmail.com>
2021-12-09 20:30:18 +0100

    Prepared submission files, i hope

--- projects/Harvardx/Capstone/Project 1 - Movielens/13 code for submission - part 2.R
+++ projects/Harvardx/Capstone/Project 1 - Movielens/13 code for submission - part 2.R
@@ -7,21 +7,110 @@ library(data.table)
 library(dplyr)
 
 # # removing all files
-# rm(list = ls())
+ # rm(list = ls())
 
 # clearing unused memory
 invisible(gc())
 # 
-# # loading the data files
+####################################################################################
+####### Running a simple linear model, with constants for users and movies #########
+####################################################################################
+
+# loading the data files
 setwd("H:/My Drive/sync/data analytics and machine learning/harvardx/Capstone/Github project/public/ml-10M100K")
-# # ratings<-readRDS("ratings")
-# # movies<-readRDS("movies")
+# ratings<-readRDS("ratings")
+# movies<-readRDS("movies")
 # movielens<-readRDS("movielens")
-core<-readRDS("core")
-sub<-readRDS("sub")
-# validation<-readRDS("validation")
-# edx<-readRDS("edx")
-rmse_results<-readRDS("rmse_results")
+# core<-readRDS("core")
+# sub<-readRDS("sub")
+validation<-readRDS("validation")
+edx<-readRDS("edx")
+
+# Validation method is "Leave One Out Cross Validation (LOOCV)"
+
+### creating a subset of the training set, for testing the model ###
+# I am avoiding calling this a validation set, 
+# since edx called the test set 'validation set'.
+# To avoid confusion, the core training set will be called 'core' and the subset of the training set
+# for validation will be called 'sub'.
+
+# The 'sub' set will be 15% of the training set
+ sub_index <- createDataPartition(y = edx$rating, times = 1, p = 0.15, list = FALSE)
+ core <- edx[-sub_index,]
+ temp <- edx[sub_index,]
+
+# Make sure userId and movieId in sub set are also in core set
+sub <- temp %>%
+  semi_join(core, by = "movieId") %>%
+  semi_join(core, by = "userId")
+
+# Add rows removed from sub set back into core set
+removed <- anti_join(temp, sub)
+core <- rbind(core, removed)
+# 
+# rm(sub_index, temp, removed)
+
+### The first model ###
+# Predicting only according to the average rating in the dataset ###
+mu <- mean(core$rating)
+mu
+
+# checking the root mean squared error
+naive_rmse <- RMSE(sub$rating, mu)
+naive_rmse
+
+# creating a results table
+rmse_results <- c("Just the average", round(naive_rmse,5))
+names(rmse_results)<-c("method", "RMSE")
+rmse_results
+
+### adding movie effects ###
+movie_avgs <- core %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = mean(rating - mu))
+
+# examining the distributions of constant movie effects
+# qplot(b_i, data = movie_avgs, bins = 10, color = I("black"))
+
+# checking the rmse
+predicted_ratings <- mu + sub %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  pull(b_i)
+movie_effects_rmse<-RMSE(predicted_ratings, sub$rating)
+
+rmse_results <- rbind.data.frame(rmse_results, c("With movie effects", round(movie_effects_rmse,5)))
+names(rmse_results)<-c("method", "RMSE")
+rmse_results
+
+### adding user effects ###
+# examining the average rating per user
+# core %>% 
+#   group_by(userId) %>% 
+#   filter(n()>=100) %>%
+#   summarize(b_u = mean(rating)) %>% 
+#   ggplot(aes(b_u)) + 
+#   geom_histogram(bins = 30, color = "black")
+
+# estimating the user effects, by computing 
+# the overall average and the item effect, and then 
+# the user effect is the average of the remainder, after they
+# are substacted from the rating (user effect= average of (rating - overall average - item effect)
+user_avgs <- core %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+# checking the rmse
+predicted_ratings <- sub %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  pull(pred)
+movie_and_user_effects_rmse<-RMSE(predicted_ratings, sub$rating)
+
+rmse_results <- rbind.data.frame(rmse_results, c("With movie and user effects", round(movie_and_user_effects_rmse,5)))
+names(rmse_results)<-c("method", "RMSE")
+rmse_results
 
 # user and movie fixed effects
 # Regularizing with lambda = 3
@@ -38,36 +127,6 @@ movie_avgs <- core %>%
   group_by(movieId) %>%
   summarize(b_i = sum(rating - mu - b_u)/(n()+lambda), n_i = n())
 
-# Making a plot to see how the estimates shrunk
-# tibble(original = movie_avgs$b_i, 
-#        regularlized = movie_reg_avgs$b_i, 
-#        n = movie_reg_avgs$n_i) %>%
-#   ggplot(aes(original, regularlized, size=sqrt(n))) + 
-#   geom_point(shape=1, alpha=0.5)
-
-### Looking up the top 10 best movies ###
-# movie_titles <- movielens %>% 
-#   select(movieId, title) %>%
-#   distinct()
-# 
-# core %>%
-#   count(movieId) %>% 
-#   left_join(movie_reg_avgs, by = "movieId") %>%
-#   left_join(movie_titles, by = "movieId") %>%
-#   arrange(desc(b_i)) %>% 
-#   slice(1:10) %>% 
-#   pull(title)
-
-### Looking up the 10 worst movies ###
-# core %>%
-#   count(movieId) %>% 
-#   left_join(movie_reg_avgs, by = "movieId") %>%
-#   left_join(movie_titles, by="movieId") %>%
-#   arrange(b_i) %>% 
-#   select(title, b_i, n) %>% 
-#   slice(1:10) %>% 
-#   pull(title)
-
 ### Calculating RMSE ###
 predicted_ratings <- sub %>% 
   left_join(movie_avgs, by='movieId') %>%
@@ -82,7 +141,7 @@ rmse_results <- c("Movie and user effects with regularization", round(movie_and_
 names(rmse_results)<-c("method", "RMSE")
 rmse_results
 
-# user and movie fixed effects
+# user, movie and genre fixed effects
 # Regularizing with lambda = 3
 lambda <- 3
 mu <- mean(core$rating)
@@ -97,9 +156,6 @@ movie_avgs <- core %>%
   left_join(user_avgs, by='userId') %>%
   group_by(movieId) %>%
   summarize(b_i = sum(rating - mu - b_u)/(n()+lambda), n_i = n())
-
-head(user_avgs)
-head(movie_avgs)
 
 # genre fixed effects with regularization
 # creating numeric genre column
@@ -129,35 +185,53 @@ rmse_results <- rbind(rmse_results, c("Movie and user effects with regularizatio
 names(rmse_results)<-c("method", "RMSE")
 rmse_results
 
+### Exploring the training set ###
+dim(edx)
+names(edx)
+head(edx)
+summary(edx)
+sum(is.na(edx)) # counting missing values
 
-# user*genre fixed effects with regularization
-# creating numeric user*genre column
-core$user_genre<-as.numeric(as.factor(core$userId))*as.numeric(as.factor(core$genres))
+# counting the unique values
+n_distinct(edx$userId) # users
+n_distinct(edx$movieId) # movies
+n_distinct(edx$rating) # ratings
+n_distinct(edx$genres) # genres
 
-user_genre_avgs <- core %>% 
-  left_join(user_avgs) %>%
-  left_join(movie_avgs) %>%
-  left_join(genre_avgs) %>%
-  group_by(user_genre) %>% 
-  summarize(b_ug = mean(rating - mu-b_u-b_i-b_g)/(n()+lambda), n_i = n())
+### examining some distributions of the ratings ###
+# distribution of the ratings
+histogram(edx$rating) 
+ratings_per_movie<-edx %>%
+  count(movieId)
 
-### Calculating RMSE ###
-# creating a numeric user_genres column in the
-# sub dataset
-sub$user_genre<-as.numeric(as.factor(sub$userId))*as.numeric(as.factor(sub$genres))
+# distribution of the ratings per movie
+histogram(ratings_per_movie$n, breaks=30)  
+boxplot(ratings_per_movie$n)
+summary(ratings_per_movie$n) 
 
-predicted_ratings <- sub %>% 
-  left_join(movie_avgs, by='movieId') %>%
-  left_join(user_avgs, by='userId') %>%
-  left_join(genre_avgs, by='genresnum') %>%
-  left_join(user_genre_avgs, by='user_genre') %>%
-  mutate(pred = mu + b_i + b_u + b_g+b_ug) %>%
-  pull(pred)
-movie_and_user_effects_with_regularization_rmse<-RMSE(predicted_ratings, sub$rating)
+rm(ratings_per_movie)
 
-# checking the RMSE
-rmse_results <- rbind(rmse_results, c("Movie and user effects with regularization", round(movie_and_user_effects_with_regularization_rmse,5)))
-names(rmse_results)<-c("method", "RMSE")
-rmse_results
+# distribution of the ratings per user
+ratings_per_user<-edx %>%
+  filter(!is.na(rating))  %>%
+  count(userId)
 
+histogram(ratings_per_user$n, breaks=30)  
+boxplot(ratings_per_user$n)
+summary(ratings_per_user$n) 
 
+rm(ratings_per_user)
+
+### Creating a matrix of ratings with userIds (rows) by movieIds (columns) ###
+trainmat<-pivot_wider(edx, id_cols = userId, names_from = title, values_from = rating, values_fn=length, names_sep = "_", names_repair = "check_unique")
+dim(trainmat)
+head(trainmat)[1:5,1:5]
+
+# exploring the matrix #
+n_missing<-sum(is.na(trainmat)) # counting mising values
+n_nonmissing<-sum(!is.na(trainmat)) # counting nonmissing values
+p_nonmissing<-n_nonmissing/(n_missing+n_nonmissing) # calculating the percentage of nonmissing values
+p_nonmissing 
+rm(n_missing, n_nonmissing, p_nonmissing) # removing these variables
+
+# rm(trainmat)
