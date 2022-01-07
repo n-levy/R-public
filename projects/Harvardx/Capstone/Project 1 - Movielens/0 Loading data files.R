43c35b97ae859cec5a3fd71a75dc598dae9ab9b6
Nir Levy <nirlevy5@gmail.com>
2022-01-07 12:02:16 +0100

    Loading data files

---
+++ projects/Harvardx/Capstone/Project 1 - Movielens/0 Loading data files.R
@@ -0,0 +1,8 @@
+######################################
+##### Loading data files #############
+######################################
+
+setwd("H:/My Drive/sync/data analytics and machine learning/harvardx/Capstone/Github project/public/ml-10M100K/data files")
+edx<-readRDS("edx")
+validation<-readRDS("validation")
+
