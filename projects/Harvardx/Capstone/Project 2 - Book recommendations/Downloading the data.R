70edf1bbb7c5611cbeba94e23ae54c1a3977d772
Nir Levy <nirlevy5@gmail.com>
2022-01-10 22:27:31 +0100

    Beginning new project

---
+++ projects/Harvardx/Capstone/Project 2 - Book recommendations/Downloading the data.R
@@ -0,0 +1,25 @@
+# Downloading the data #
+
+if(!require(downloader)) install.packages("downloader", repos = "http://cran.us.r-project.org")
+
+# install.packages("downloader")
+library(downloader)
+
+# downloading
+download("http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip", dest="dataset.zip", mode="wb") 
+unzip ("dataset.zip")
+
+#converting the files to data frame format
+books<-read.csv(file = 'BX-Books.csv', sep=";")
+users<-read.csv(file = 'BX-Users.csv', sep=";")
+ratings<-read.csv(file = 'BX-Book-Ratings.csv', sep=";")
+
+# saving
+saveRDS(books, "books")
+saveRDS(users, "users")
+saveRDS(ratings, "ratings")
+
+# exploring
+dim(books)
+dim(users)
+dim(ratings)
